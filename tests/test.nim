c042f3fa199d8bf1303bc19febd9f7dba5343a47
Nickolay Bukreyev <buknik95@yandex.ru>
2023-03-02 20:04:56 +0700

    Implement `asyncIter` macro

--- tests/test.nim
+++ tests/test.nim
@@ -1,6 +1,12 @@
 import std/unittest
 import asyncIters
 
+test "can declare an async iterator":
+  iterator named0: Future[int] {.asyncIter.} = discard
+  iterator named1(): Future[int] {.asyncIter.} = discard
+  let unnamed0 = iterator: Future[int] {.asyncIter.} = discard
+  let unnamed1 = iterator (): Future[int] {.asyncIter.} = discard
+
 test "can await an iterator":
   var s = 0
   for i in awaitIter 0 .. 3:
